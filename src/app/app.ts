2062e5a5bfe4504002525ad1023daebd01a8d72a
joao-lucas <joaolucasdcoliveira@gmail.com>
2026-07-16 15:40:45 -0300

    refactor: adicionado um novo botao para executar um comando

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';//!Remove importação do routerOutlet, pois não estou usando ele
-import { Produto } from './components/produto/produto'; //importa o componente
-import { ListaProdutos } from './components/lista-produtos/lista-produtos'; 
+import { Produto } from './features/produtos/produto/produto'; //importa o componente
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos'; 
 @Component({
  selector: 'app-root',
   imports: [ListaProdutos],
